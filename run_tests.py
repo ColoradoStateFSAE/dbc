2cf6901e3a212de318d015f422d6b95185e3db0c
Jonathan Christensen <27783364+jonathan-christensen@users.noreply.github.com>
2025-03-04 22:08:04 -0700

    Test coverage

--- run_tests.py
+++ run_tests.py
@@ -14,7 +14,7 @@ def compare_signals(actual, expected):
 
 if __name__ == "__main__":
     loader = unittest.TestLoader()
-    suite = loader.discover("tests")
+    suite = loader.discover("tests_r3")
 
     runner = unittest.TextTestRunner()
     runner.run(suite)
