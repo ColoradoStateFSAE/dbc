00cacd218e290a8809eae7cea22296b5305037ad
Jonathan Christensen <27783364+jonathan-christensen@users.noreply.github.com>
2025-07-19 22:15:18 -0600

    2026

--- src/can_macros.h
+++ src/can_macros.h
@@ -1,89 +1,46 @@
 #pragma once
 
-/**
- * @brief Creates a dbc_message_t struct initialized with zeros.
- *
- *
- * @param message The name of a message.
- */
 #define INIT_MESSAGE(message) \
     message##_t message; \
     message##_init(&message)
 
-/**
- * @brief Encodes a signal into a dbc_message_t struct.
- *
- *
- * @param message The name of a message.
- * @param signal The name of a signal.
- * @param value Value to be encoded using the signal format.
- */
-#define ENCODE_SIGNAL(message, signal, value) \
-    message.signal = message##_##signal##_encode(value)
-
-/**
- * @brief Decodes a signal from a dbc_message_t struct.
- *
- *
- * @param message The name of a message.
- * @param signal The name of a signal.
- * @return The decoded value.
- */
-#define DECODE_SIGNAL(message, signal) \
-    message##_##signal##_decode(message.signal)
-
-/**
- * @brief Pack message.
- *
- *
- * @param message The name of a message.
- * @param dst Object to pack the message into.
- */
 #define PACK_MESSAGE(message, dst) \
     message##_pack(dst, &message, sizeof(dst))
 
-/**
- * @brief Unpack message.
- *
- *
- * @param message The name of a message.
- * @param dst Object to unpack the message into.
- */
 #define UNPACK_MESSAGE(message, src) \
     message##_unpack(&message, src, sizeof(src))
 
-/**
- * @brief Check that given signal is in allowed range.
- *
- *
- * @param message The name of a message.
- * @param signal The name of a signal.
- */
+#define ENCODE_SIGNAL(message, signal, value) \
+    message.signal = message##_##signal##_encode(value)
+
+#define DECODE_SIGNAL(message, signal) \
+    message##_##signal##_decode(message.signal)
+
 #define IS_IN_RANGE(message, signal) \
     message##_##signal##_is_in_range(message.signal)
 
-/**
- * @brief A function name to read a message.
- *
- *
- * @param message The name of a message.
- */
-#define READ_MESSAGE(message) \
-    read_##message(const message##_t &message)
+#if defined(RASPBERRY_PI_PICO_2) || defined(TEST_PICO_2)
 
+#define DECODE_METHOD(message) \
+    decode_##message(const message##_t &message)
 
-#ifdef TEST_PICO_2
+#define ENCODE_METHOD(message) \
+    encode_##message(message##_t &message)
 
-/**
- * @brief Creates a if statement to call an accompanying READ_MESSAGE() function.
- *
- *
- * @param frame The uppercase name of a message.
- * @param message The name of a message.
- * @param src Object to unpack into the message.
- */
+#define INIT_FRAME(frame, message) \
+    struct can_frame message##_msg; \
+    message##_msg.can_id = frame##_FRAME_ID | (frame##_IS_EXTENDED ? CAN_EFF_FLAG : 0); \
+    message##_msg.can_dlc = frame##_LENGTH
 
-#define FRAME_MASK(is_extended) ((is_extended) ? CAN_EFF_MASK : CAN_SFF_MASK)
+#define CYCLIC_TRANSMITTER(frame, message) \
+    timers.setInterval([&](){ \
+        /* CAN frame ID and DLC */ \
+        INIT_FRAME(frame, message); \
+        INIT_MESSAGE(message); \
+        encode_##message(message); \
+        PACK_MESSAGE(message, message##_msg.data); \
+        mcp.sendMessage(&message##_msg); \
+    }, frame##_CYCLE_TIME_MS)
 
 #define READ_MESSAGE_CASE(frame, message) \
     case frame##_FRAME_ID | (frame##_IS_EXTENDED ? CAN_EFF_FLAG : 0): { \
@@ -92,30 +49,5 @@
         decode_##message(message); \
         break; \
     }
-
-#define INIT_FRAME(message, frame) \
-    struct can_frame message##_msg; \
-    message##_msg.can_id = frame##_FRAME_ID | (frame##_IS_EXTENDED ? CAN_EFF_FLAG : 0); \
-    message##_msg.can_dlc = frame##_LENGTH
-
-#define DECODE_METHOD(message) \
-    decode_##message(const message##_t &message)
-
-#define ENCODE_METHOD(message) \
-    encode_##message(message##_t &message)
-
-#define CYCLIC_TRANSMIT_METHOD(frame, message) \
-    void ENCODE_METHOD(message); \
-    AsyncTimer message##_timer = [&]() { \
-        AsyncTimer timer; \
-        timer.setInterval([&](){ \
-            /* CAN frame ID and DLC */ \
-            INIT_FRAME(message, frame); \
-            INIT_MESSAGE(message); \
-            encode_##message(message); \
-            PACK_MESSAGE(message, message##_msg.data); \
-        }, frame##_CYCLE_TIME_MS); \
-        return timer; \
-    }()
 
 #endif
