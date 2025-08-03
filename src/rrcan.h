9f92217714244360dc3bbaac06cc9e92d3480c40
Jonathan Christensen <27783364+jonathan-christensen@users.noreply.github.com>
2025-08-03 08:50:58 -0600

    2026

--- src/rrcan.h
+++ src/rrcan.h
@@ -1,7 +1,12 @@
 #pragma once
 
 #include "can_macros.h"
-#include "display.h"
 #include "haltech.h"
 #include "swc.h"
 #include "tcs.h"
+#include "display.h"
+
+#include "haltech_aliases.h"
+#include "swc_aliases.h"
+#include "tcs_aliases.h"
+#include "display_aliases.h"
